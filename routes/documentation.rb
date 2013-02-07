657b3723b63ee30f1b7382c92721b39de782066a
Hudson <hudson@instructure.com>
2013-02-07 02:32:28 +0000

    removing files that should have been moved

--- routes/documentation.rb
+++
@@ -1,9 +0,0 @@
-class Portal < Sinatra::Application
-  post "/documentation" do
-    execution_time = Benchmark.realtime do
-      Writer.write_info('generate documentation')
-      system("ruby /home/hudson/canvas-lms/branch_tools.rb -g 'true'")
-    end
-    ActionTime.store_action_time('documentation', execution_time) if Validation.check_error_file
-  end
-end
